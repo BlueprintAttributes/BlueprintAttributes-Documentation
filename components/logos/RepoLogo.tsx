5f78bd8c1c855c00259861bf2cb2337e56c42295
Mickael Daniel <daniel.mickael@gmail.com>
2023-09-15 04:25:30 +0200

    Setup next config to export as static site

--- components/logos/RepoLogo.tsx
+++ components/logos/RepoLogo.tsx
@@ -1,6 +1,6 @@
 const RepoLogo = (props) => (
   <>
-    <h1 style={{ fontFamily: 'Montserrat, sans-serif;' }} className="text-3xl font-bold tracking-tighter uppercase">Blueprint Attributes</h1>
+    <h1 style={{ fontFamily: 'Montserrat, sans-serif' }} className="text-3xl font-bold tracking-tighter uppercase">Blueprint Attributes</h1>
   </>
 );
 
--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx'
-})
- 
-module.exports = withNextra()
- 
+});
+
+module.exports = {
+  ...withNextra({
+    output: 'export',
+    images: {
+      unoptimized: true
+    },
+  })
+}
+
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
