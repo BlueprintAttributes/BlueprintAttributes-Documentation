4427c90dd448810ad6eae2fa7c3eca894520bf80
Mickael Daniel <daniel.mickael@gmail.com>
2024-04-02 19:50:00 +0200

    Add umani script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,5 +6,8 @@ import '../styles/zoom.css'
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
 
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+    <script defer src="https://analytics.eu.umami.is/script.js" data-website-id="9108253b-bd4d-49be-87a3-9689be313969"></script>
+  </>
 }
